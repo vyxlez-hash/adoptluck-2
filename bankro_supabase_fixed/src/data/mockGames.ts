dfeeac7c44c4acdbbfafd3d3e7adefbc7abd67a2
reverse <vyxlez@icloud.com>
2026-09-09 01:06:22 +0500

    Delete bankro_supabase_fixed directory

--- bankro_supabase_fixed/src/data/mockGames.ts
+++
@@ -1,7 +0,0 @@
-import { CoinflipGame } from '../types';
-
-/**
- * No demo fake users or fake bots.
- * Games are created dynamically by real users and saved persistently.
- */
-export const INITIAL_GAMES: CoinflipGame[] = [];
